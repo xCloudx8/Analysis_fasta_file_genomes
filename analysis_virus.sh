8d03988b72d82585276fa4200d752e284282dd22
xCloudx8 <danielestrife@gmail.com>
2016-03-26 12:39:59 +0100

    Updated
    Updated file

--- analysis_virus.sh
+++ analysis_virus.sh
@@ -6,7 +6,7 @@ VIRUSES=(
   "Enterobacteria_phage_phiX174_sensu_lato"
   "Enterobacteria_phage_T4"
   "Bacillus_phage_phi29"
-  #"Enterobacteria_phage_T5"
+  "Enterobacteria_phage_T5"
   "Sulfolobus_spindle-shaped_virus_4"
   "Enterobacteria_phage_M13"
   "Sulfolobus_islandicus_rod-shaped_virus_1"
@@ -23,25 +23,20 @@ VIRUSES=(
 for f in "${VIRUSES[@]}"
 	do
     echo "Done with GenomeStats for: " $f
-    java -cp IGTools_cli.jar igtools.cli.GenomeStats /home/daniele/Documents/Project/Virus/Virus_3Bit/"$f".3bit /home/daniele/Documents/Project/Virus/Virus_nelsa/"$f".nelsa df -h | sed 's/  */\t/g' > /home/daniele/Documents/Project/Reports/Virus_Reports/"$f".csv
+    java -cp IGTools_cli.jar igtools.cli.GenomeStats /Project/Virus/Virus_3Bit/"$f".3bit /home/daniele/Documents/Project/Virus/Virus_nelsa/"$f".nelsa df -h | sed 's/  */\t/g' > /Project/Reports/Virus_Reports/"$f".csv
 	done
 
-cd /home/daniele/Documents/Project/Reports/Virus_Reports
-
 ./order.py
-
-cd /home/daniele/Documents/Project
-
 echo "Ordered"
 
 for f in "${VIRUSES[@]}"
 do
    echo "Please wait GenomeKStats for: " $f
-   java -cp IGTools_cli.jar igtools.cli.GenomeKStats "6" "24" /home/daniele/Documents/Project/Virus/Virus_3Bit/"$f".3bit /home/daniele/Documents/Project/Virus/Virus_nelsa/"$f".nelsa df -h | sed 's/  */\t/g' > /home/daniele/Documents/Project/Reports/Virus_Reports/FromK/"$f".csv
+   java -cp IGTools_cli.jar igtools.cli.GenomeKStats "6" "24" /Project/Virus/Virus_3Bit/"$f".3bit /Project/Virus/Virus_nelsa/"$f".nelsa df -h | sed 's/  */\t/g' > /Project/Reports/Virus_Reports/FromK/"$f".csv
 done
 
-rm /home/daniele/Documents/Project/Reports/Virus_distr/mdistr/*.mdistr
-rm /home/daniele/Documents/Project/Reports/Virus_distr/minmaxavg/*.csv
+rm /Project/Reports/Virus_distr/mdistr/*.mdistr
+rm /Project/Reports/Virus_distr/minmaxavg/*.csv
 echo "Removed all mdistr"
 
 for f in "${VIRUSES[@]}"
@@ -49,15 +44,15 @@ for f in "${VIRUSES[@]}"
    for m in {6..24}
 		do
 	 	 echo   "Analizing MultiplicityDistribution: "$f " " $m
-     echo "-------------------------------------------------------------------------k='$m'" >> /home/daniele/Documents/Project/Reports/Virus_distr/mdistr/"$f".mdistr
-     java -cp IGTools_cli.jar igtools.cli.distributions.MultiplicityDistribution $m "a" /home/daniele/Documents/Project/Virus/Virus_3Bit/"$f".3bit /home/daniele/Documents/Project/Virus/Virus_nelsa/"$f".nelsa | awk '{min=9999}; /#/ {num+=1; tot+=$3; if ($3<min) min=$3; if ($3>max) max=$3} END{ print  min " " tot/num  "  " max }' >> /home/daniele/Documents/Project/Reports/Virus_distr/minmaxavg/"$f"min_avg_max.csv
-     echo " " >> /home/daniele/Documents/Project/Reports/Virus_distr/mdistr/"$f".mdistr
-		 java -cp IGTools_cli.jar igtools.cli.distributions.MultiplicityDistribution $m "a" /home/daniele/Documents/Project/Virus/Virus_3Bit/"$f".3bit /home/daniele/Documents/Project/Virus/Virus_nelsa/"$f".nelsa >> /home/daniele/Documents/Project/Reports/Virus_distr/mdistr/"$f".mdistr
+     echo "-------------------------------------------------------------------------k='$m'" >> /Project/Reports/Virus_distr/mdistr/"$f".mdistr
+     java -cp IGTools_cli.jar igtools.cli.distributions.MultiplicityDistribution $m "a" /Project/Virus/Virus_3Bit/"$f".3bit /Project/Virus/Virus_nelsa/"$f".nelsa | awk '{min=9999}; /#/ {num+=1; tot+=$3; if ($3<min) min=$3; if ($3>max) max=$3} END{ print  min " " tot/num  "  " max }' >> /Project/Reports/Virus_distr/minmaxavg/"$f"min_avg_max.csv
+     echo " " >> /Project/Reports/Virus_distr/mdistr/"$f".mdistr
+		 java -cp IGTools_cli.jar igtools.cli.distributions.MultiplicityDistribution $m "a" /home/daniele/Documents/Project/Virus/Virus_3Bit/"$f".3bit /home/daniele/Documents/Project/Virus/Virus_nelsa/"$f".nelsa >> /Project/Reports/Virus_distr/mdistr/"$f".mdistr
 	 done
 done
-#Calc min_Max_Avg
 
-rm /home/daniele/Documents/Project/Reports/Virus_distr/rldistr/*.rldistr
+
+rm /Project/Reports/Virus_distr/rldistr/*.rldistr
 echo "Removed all rldistr"
 
 for f in "${VIRUSES[@]}"
@@ -69,14 +64,11 @@ for f in "${VIRUSES[@]}"
 	for m in {1..100}
 		do
 		 echo  "Analizing RepeatLengthDistribution: "$f " " $m
-     echo "------------------------------------------------------------------------- k='$m'" >> /home/daniele/Documents/Project/Reports/Virus_distr/rldistr/"$f".rldistr
+     		 echo "------------------------------------------------------------------------- k='$m'" >> /home/daniele/Documents/Project/Reports/Virus_distr/rldistr/"$f".rldistr
 	 	 java -cp IGTools_cli.jar igtools.cli.distributions.RepeatLengthDistribution $m "a" /home/daniele/Documents/Project/Virus/Virus_3Bit/"$f".3bit /home/daniele/Documents/Project/Virus/Virus_nelsa/"$f".nelsa >> /home/daniele/Documents/Project/Reports/Virus_distr/rldistr/"$f".rldistr
 		done
 		 sed -i '1 i\k	|D_k|	|H_k|	|R_k|	|T_k|	|E_k|' /home/daniele/Documents/Project/Reports/Virus_Reports/FromK/"$f".csv
 	done
 
-cd /home/daniele/Documents/Project/Reports/Virus_Reports
-
 ./concatenating_virus.py
 
-cd /home/daniele/Documents/Project
