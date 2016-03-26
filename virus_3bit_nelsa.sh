6a0901736e3f11edad0b06ac91dd45db933f27f1
xCloudx8 <danielestrife@gmail.com>
2016-03-26 12:43:34 +0100

    Updated
    Updated file

--- virus_3bit_nelsa.sh
+++ virus_3bit_nelsa.sh
@@ -2,16 +2,16 @@
 
 VIRUSES=(
 
-#"Pseudomonas_phage_phi-6_segment_L"
-#"Enterobacteria_phage_phiX174_sensu_lato"
-#"Enterobacteria_phage_T4"
-#"Bacillus_phage_phi29"
-#"Enterobacteria_phage_T5"
-#"Sulfolobus_spindle-shaped_virus_4"
-#"Enterobacteria_phage_M13"
-#"Sulfolobus_islandicus_rod-shaped_virus_1"
-#"Enterobacteria_phage_T7"
-#"Escherichia_phage_P13374"
+"Pseudomonas_phage_phi-6_segment_L"
+"Enterobacteria_phage_phiX174_sensu_lato"
+"Enterobacteria_phage_T4"
+"Bacillus_phage_phi29"
+"Enterobacteria_phage_T5"
+"Sulfolobus_spindle-shaped_virus_4"
+"Enterobacteria_phage_M13"
+"Sulfolobus_islandicus_rod-shaped_virus_1"
+"Enterobacteria_phage_T7"
+"Escherichia_phage_P13374"
 "Acidianus_filamentous_virus_1"
 "Acidianus_two-tailed_virus"
 "Halo_virus_PH1"
@@ -26,16 +26,16 @@ VIRUSES=(
 
 for f in "${VIRUSES[@]}"
 do
-   java -cp IGTools_cli.jar igtools.cli.util.FASTATo3bit /home/daniele/Documents/Project/Virus/"$f".fna "$f".3bit
-   mv "$f.3bit" /home/daniele/Documents/Project/Virus/Virus_3Bit
+   java -cp IGTools_cli.jar igtools.cli.util.FASTATo3bit /Project/Virus/"$f".fna "$f".3bit
+   mv "$f.3bit" /Project/Virus/Virus_3Bit
    echo "$f done"
    echo ""
 done
 
 for f in "${VIRUSES[@]}"
 do
-   java -cp IGTools_cli.jar igtools.cli.dictionaries.BuildNELSA /home/daniele/Documents/Project/Virus/Virus_3Bit/"$f".3bit "$f".nelsa
-   mv "$f.nelsa" /home/daniele/Documents/Project/Virus/Virus_nelsa
+   java -cp IGTools_cli.jar igtools.cli.dictionaries.BuildNELSA /Project/Virus/Virus_3Bit/"$f".3bit "$f".nelsa
+   mv "$f.nelsa" /Project/Virus/Virus_nelsa
    echo "$f done"
    echo ""
 done
